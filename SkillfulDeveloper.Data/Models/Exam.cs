7b64671d6824cbddd7727998eddfda173002fe9b
Siniša <Benceti>
2015-05-27 21:50:25 +0200

    Data project with NPoco

---
+++ SkillfulDeveloper.Data/Models/Exam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco;
+
+namespace SkillfulDeveloper.Data.Models
+{
+    [TableName("Exam")]
+    [PrimaryKey("Id")]
+    public class Exam
+    {        
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
---
+++ SkillfulDeveloper.Data/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillfulDeveloper.Data.Models
+{
+    [TableName("Tag")]
+    [PrimaryKey("Name")]
+    public class Tag
+    {
+        public string Name { get; set; }
+    }
+}
